03f492c6924cf730586030090a90a60b8f004e47
junwoo <qkrdnjs1201@naver.com>
2018-11-15 13:21:36 +0900

    qwer

---
+++ src/intlong/page100.java
@@ -0,0 +1,24 @@
+package intlong;
+
+public class page100 {
+
+	public static void main(String[] args) {
+		int grade = 70;
+		char ch1 ;
+		
+		if(grade>=90) {
+			ch1 = 'a';
+			System.out.println("학점은"+ch1+"입니다");
+			
+		}
+		else if(grade>=80) {
+			ch1 = 'B';
+			System.out.println("학점은"+ch1+"입니다");
+			
+		}
+		else ch1 = 'C';
+			System.out.println("학점은"+ch1+"입니다");
+			
+	}
+
+}
---
+++ src/intlong/page106.java
@@ -0,0 +1,21 @@
+package intlong;
+
+public class page106 {
+
+	public static void main(String[] args) {
+			int floor = 5 ;
+		switch(floor) {
+		case 1 : System.out.println("1층약국입니다");
+		break;
+		case 2 : System.out.println("2층정형외과입니다");
+		break;
+		case 3 : System.out.println("3층피부과입니다");
+		break;
+		case 4 :System.out.println("4층치과입니다");
+		break;
+		default :System.out.println("5층헬스클럽입니다");
+		break;
+		
+	}
+	}
+}
